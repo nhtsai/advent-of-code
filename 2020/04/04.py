55b614280fc146f18d8e51b8271f6c00d25c8f12
Nathan Tsai <natesigh@gmail.com>
2020-12-06 02:40:26 -0800

    remove comments in 04

--- 2020/04/04.py
+++ 2020/04/04.py
@@ -5,19 +5,6 @@ def part1(l):
     return sum([all(m in p for m in matches) for p in l])
 
 def part2(l):
-    # pattern = r"""(?=.*byr:(19[2-9][0-9]|200[0-2]))(?=.*iyr:(201[0-9]|2020))(?=.*eyr:(202[0-9]|2030))(?=.*hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in))(?=.*hcl:(#[0-9a-f]{6}))(?=.*ecl:([a-z]{3}))(?=.*pid:([0-9]{9}))"""
-    # valid = 0
-    # for p in l:
-    #     s = re.search(pattern, p)
-    #     if s is not None:
-    #         byr, iyr, eyr, whole, inch, cm, hcl, ecl, pid = [x for x in s.groups()]
-    #         print(whole, inch, cm, hcl)
-    #         byr, iyr, eyr = int(byr), int(iyr), int(eyr)
-    #         hgt = int(inch) if inch else int(cm)
-
-    #         if valid == 3: break
-    # return valid
-
     checks = {
         'byr': re.compile("byr:(19[2-9][0-9]|200[0-2])"),
         'iyr': re.compile("iyr:(201[0-9]|2020)"),
