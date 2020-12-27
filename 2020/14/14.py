e0ad406a51b047c081b3f00c7a0eeb94911118b7
Nathan Tsai <natesigh@gmail.com>
2020-12-27 03:54:38 -0800

    update 14

--- 2020/14/14.py
+++ 2020/14/14.py
@@ -1,6 +1,4 @@
 import re
-from collections import defaultdict
-import time
 
 def part1(l):
     memory = {}
@@ -22,12 +20,13 @@ def part1(l):
             # write result into memory
             memory[address] = int(result, 2)
         i += 1
+    # return sum of values in memory
     return sum(memory.values())
 
 
 def part2(l):
     idx = 0
-    memory = defaultdict(list)
+    memory = {}
     while idx < len(l):
         # process group
         if 'mask' in l[idx]:
@@ -36,23 +35,29 @@ def part2(l):
         elif 'mem' in l[idx]:
             # process memory operation
             m = re.match(r"mem\[([0-9]+)\] = ([0-9]+)$", l[idx])
-            # calculate all addresses
+            # convert address to floating address
             address = int(m.group(1))
             binary_address_str = format(address, 'b').zfill(len(mask))
             address_result = ("").join(['X' if i == 'X' else '1' if i == '1' else j for i,j in zip(mask, binary_address_str)])
+            # get all addresses
             n = address_result.count('X')
             address_result = address_result.replace('X', '%s')
             addresses = []
             for c in range(2**n):
+                # get floating bit combination
                 floating = tuple(list(str(bin(c))[2:].zfill(n)))
+                # fill in combination in address
                 address_str = address_result % floating
+                # add address to list as a decimal
                 addresses.append(int(address_str, 2))
+            # write value into all addresses
             value = int(m.group(2))
-            # write result into all addresses
             for a in addresses:
                 memory[a] = value
         idx += 1
+    # return sum of values in memory
     return sum(memory.values())
+
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
