8a1a6487145224c828d90724c996f896ebd7b4e9
Nathan Tsai <natesigh@gmail.com>
2021-12-01 17:38:58 -0800

    add 01-2021

---
+++ 2021/01.py
@@ -0,0 +1,26 @@
+def part1(l):
+    inc = 0
+    for i in range(1, len(l)):
+        if l[i-1] < l[i]:
+            inc += 1
+    return inc
+
+def part2(l):
+    inc = 0
+    k = 3
+    for i in range(k, len(l)):
+        if l[i-k] < l[i]:
+            inc += 1
+    return inc
+
+def test():
+    l = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert part1(l) == 7
+    assert part2(l) == 5
+
+
+if __name__ == '__main__':
+    with open('./data/input-01.txt', 'r') as f:
+        l = list(map(int, f.readlines()))
+    print("Part 1:", part1(l))
+    print("Part 2:", part2(l))
