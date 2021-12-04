7bead8690c8b7722ea43e004f859ad06bf770036
Nathan Tsai <natesigh@gmail.com>
2021-12-03 17:27:18 -0800

    update 2021-01

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,19 +1,11 @@
 from pathlib import PurePath
 
 def part1(l):
-    inc = 0
-    for i in range(1, len(l)):
-        if l[i-1] < l[i]:
-            inc += 1
-    return inc
+    return sum(l[i-1] < l[i] for i in range(1, len(l)))
 
 def part2(l):
-    inc = 0
     k = 3
-    for i in range(k, len(l)):
-        if l[i-k] < l[i]:
-            inc += 1
-    return inc
+    return sum(l[i-k] < l[i] for i in range(k, len(l)))
 
 def test():
     l = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
@@ -23,6 +15,6 @@ def test():
 
 if __name__ == '__main__':
     with open(f'./data/input-{PurePath(__file__).stem}.txt', 'r') as f:
-        l = list(map(int, f.readlines()))
+        l = list(map(int, f.read().splitlines()))
     print("Part 1:", part1(l))
     print("Part 2:", part2(l))
