a34a00b466c7dd68cbb7e1469623257bce5f23f7
Nathan Tsai <natesigh@gmail.com>
2021-12-17 23:03:13 -0800

    check in 2020 work

---
+++ 2020/17/17.py
@@ -0,0 +1,11 @@
+def part1(l):
+    pass
+
+def part2(l):
+    pass
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        l = list(f.readlines())
+    print("Part 1:", part1(l))
+    print("Part 2:", part2(l))
