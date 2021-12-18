080ca9c9518e24a084c32f4ce7b2e4c701b8ca53
Nathan Tsai <natesigh@gmail.com>
2021-12-17 22:42:31 -0800

    add typing to template

--- day00.py
+++ day00.py
@@ -1,14 +1,17 @@
 from pathlib import PurePath
 import pytest
 
-def part1(l):
+def part1(l: list[int]) -> int:
     pass
 
-def part2(l):
+def part2(l: list[int]) -> int:
     pass
+
+def test() -> None:
+    assert 1 == 1
 
 if __name__ == '__main__':
-    with open(f'input-{PurePath(__file__).stem}.txt', 'r') as f:
-        l = list(f.readlines())
+    with open(f'./data/input-{PurePath(__file__).stem}.txt', 'r') as f:
+        l = f.read().splitlines()
     print("Part 1:", part1(l))
     print("Part 2:", part2(l))
